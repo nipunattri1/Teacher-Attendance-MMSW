6f3fc7587e7e0294879c4e3553af833f6d963a51
unknown <nipunattri64@gmail.com>
2022-05-15 00:50:08 +0530

    restore

---
+++ web/firebase_config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyCsmdE60tgw5WHhNDqqk1RGjPl3nSrDxpQ",
+    authDomain: "teacher-attendance-9d519.firebaseapp.com",
+    projectId: "teacher-attendance-9d519",
+    storageBucket: "teacher-attendance-9d519.appspot.com",
+    messagingSenderId: "671603227468",
+    appId: "1:671603227468:web:640aa56175921946856de9",
+    measurementId: "G-3DR2N7T43L"
+};
